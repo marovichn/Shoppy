c2c632cd9a27bd85ee590c4b3f7eca92bb43b903
marovichn <nikolamarovic.11@gmail.com>
2023-11-08 06:05:38 +0900

    Shoppy CMS - v1.0.14 - Finish

--- app/api/[storeId]/checkout/route.tsx
+++ app/api/[storeId]/checkout/route.tsx
@@ -5,7 +5,7 @@ import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "no-cors",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
