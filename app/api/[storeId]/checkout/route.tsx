766a4a932ce56985f6cefc2601e937bb917407c1
marovichn <nikolamarovic.11@gmail.com>
2023-11-08 07:29:46 +0900

    Shoppy CMS - v1.0.19 - Finish

--- app/api/[storeId]/checkout/route.tsx
+++ app/api/[storeId]/checkout/route.tsx
@@ -4,14 +4,8 @@ import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
-
 export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
+  return NextResponse.json({});
 }
 
 export async function POST(
@@ -77,7 +71,5 @@ export async function POST(
     },
   });
 
-  return NextResponse.json({ url: session.url }, {
-    headers: corsHeaders
-  });
+  return NextResponse.json({ url: session.url });
 };
--- next.config.js
+++ next.config.js
@@ -6,11 +6,10 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
