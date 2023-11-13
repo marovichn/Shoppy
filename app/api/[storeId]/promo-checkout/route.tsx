4fdc02ef2e2a008ec984d8162a5bb52ba9ad565f
marovichn <nikolamarovic.11@gmail.com>
2023-11-13 10:10:13 +0900

    Shoppy CMS - v1.1.8 - Finish

--- app/api/[storeId]/promo-checkout/route.tsx
+++ app/api/[storeId]/promo-checkout/route.tsx
@@ -28,7 +28,7 @@ export async function POST(
 
   const productsRaw =await Promise.all([
     productIds.forEach(async(id: string) => {
-      await prismadb.product.findMany({
+      return await prismadb.product.findMany({
         where: {
           id: id,
         },
