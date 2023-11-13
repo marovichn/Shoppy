2c286d2f7bf731be1f80cf9f365ad0009353e146
marovichn <nikolamarovic.11@gmail.com>
2023-11-13 09:00:33 +0900

    Shoppy CMS - v1.1.1 - Finish

---
+++ app/api/[storeId]/promo-checkout/route.tsx
@@ -0,0 +1,96 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  const { productIds, promocode } = await req.json();
+
+  if (!productIds || productIds.length === 0 || !promocode) {
+    return new NextResponse("Product ids are required, or no promotion", {
+      status: 400,
+    });
+  }
+
+  const productsRaw = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const products = productsRaw.map((product: any) => {
+    return {
+      ...product,
+      price:
+        product.price - product.price * Number(promocode.discountPercentAmount),
+    };
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach((product) => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: "USD",
+        product_data: {
+          name: product.name,
+        },
+        unit_amount: product.price.toNumber() * 100,
+      },
+    });
+  });
+
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    billing_address_collection: "required",
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `https://shoppy-shop.vercel.app/cart?success=1`,
+    cancel_url: `https://shoppy-shop.vercel.app/cart?canceled=1`,
+    metadata: {
+      orderId: order.id,
+    },
+  });
+
+  return NextResponse.json(
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    }
+  );
+}
